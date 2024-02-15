5f8d02d288579f266ef106f939d43f5f53f63f38
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-15 12:03:02 -0300

    guardando

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,89 @@
 const Formulario = () => {
+
   return (
-    <div>Desde Formulario.jsx</div>
+
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-extrabold text-3xl text-center">
+        Seguimiento de Pacientes
+      </h2>
+      <p className="font-bold text-lg text-center mt-2">
+        Añade tus pacientes y {""}
+        <span className="text-indigo-600">Adiministralos</span>
+      </p>
+
+      <form className="bg-white shadow-md p-5 rounded-md my-10">
+        <div className="mb-5">
+          <label 
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >Nombre de la mascota</label>
+          <input 
+            type="text" 
+            name="mascota" 
+            id="mascota" 
+            placeholder="Nombre de la mascota" 
+            className="border-2 p-2 w-full mt-2 rounded-md focus:outline-none placeholder-gray-400"
+          />
+        </div>
+        <div className="mb-5">
+          <label 
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >Nombre del Dueño</label>
+          <input 
+            type="text" 
+            name="propietario" 
+            id="propietario" 
+            placeholder="Nombre del dueño" 
+            className="border-2 p-2 w-full mt-2 rounded-md focus:outline-none placeholder-gray-400"
+          />
+        </div>
+        <div className="mb-5">
+          <label 
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >E-mail de Contacto</label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="E-mail de contacto" 
+            className="border-2 p-2 w-full mt-2 rounded-md focus:outline-none placeholder-gray-400"
+          />
+        </div>
+        <div className="mb-5">
+          <label 
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >Fecha del Alta</label>
+          <input 
+            type="date" 
+            name="alta" 
+            id="alta" 
+            className="border-2 p-2 w-full mt-2 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label 
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >Síntomas</label>
+          <textarea 
+            name="sintomas" 
+            id="sintomas" 
+            className="border-2 p-2 w-full mt-2 rounded-md focus:outline-none placeholder-gray-400 h-44 resize-none"
+            placeholder="¿Qué síntomas presenta el paciente?"
+          ></textarea>
+        </div>
+
+        <input 
+          type="sumbit" 
+          value="Agregar Paciente" 
+          className="bg-indigo-600 text-center w-full p-1 hover:bg-indigo-700 transition-all text-white text-2xl font-medium cursor-pointer rounded-md uppercase"
+        />
+      </form>
+
+    </div>
   )
 }
 
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,16 @@
 const ListadoPacientes = () => {
 
   return (
-    <h2>Listado Pacientes</h2>
+
+    <div className="md:w-1/2 lg:w-3/5">
+      <h1 className="text-3xl font-extrabold text-center">
+        Listado Pacientes
+      </h1>
+      <p className="font-bold text-lg text-center mt-2">
+        Administra tus pacientes y {""}
+        <span className="text-indigo-600">citas</span>
+      </p>
+    </div>
   )
 }
 
