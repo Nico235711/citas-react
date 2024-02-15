e71c92c13692b39fbe400109b407f1325e126a0f
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-15 11:10:06 -0300

    modificando el content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{html,js,jsx}"
+    "index.html",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
