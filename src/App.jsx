a52ad35f09d02073090d6c0194d631392b585d51
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-08 21:00:50 -0300

    agregando comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
   /* 
-    obtengo los pacientes del local storage y no hay nada devuelve un arreglo
-    vacio
+    obtengo el valor asociado a la clave de pacientes en el local storage y uso el operador de fusión nula (nullish coalescing operator) para proporcionar un valor predeterminado en caso de que el resultado de la operación anterior sea null o undefined
   */
   const pacienteLS = JSON.parse(localStorage.getItem("pacientes")) ?? []
 
+  // manejo de estados
   const [pacientes, setPacientes] = useState(pacienteLS)
   const [paciente, setPaciente] = useState({})
 
@@ -17,14 +17,15 @@ function App() {
     localStorage.setItem("pacientes", JSON.stringify(pacientes))
   }, [pacientes]);
 
-  /* 
-    elimina un usuario por el id
-  */
   const eliminarPaciente = id => {
     // console.log(id);
+    // Muestra una ventana de confirmación al usuario con el mensaje
     const respuesta = confirm("¿Desea elimina al paciente?")
 
     if (respuesta) {
+      /* 
+        utiliza el método filter para crear un nuevo array llamado pacientesActualizados, que contiene todos los pacientes cuyo ID no coincide con el ID proporcionado como argumento a la función
+      */
       const pacientesActualizados = pacientes.filter(pacienteState => 
         pacienteState.id !== id
       )
